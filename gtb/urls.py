ae41f13e0e61bede30366fcecfbdd8622781394d
Francisco Quiñonez <Mr.qnns008@gmail.com>
2024-04-05 06:43:56 -0500

    update load static

--- gtb/urls.py
+++ gtb/urls.py
@@ -1,9 +1,13 @@
+
 from django.contrib import admin
 from django.urls import path
 from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home),  # Actualiza la ruta para la vista home
+    #frontend
+    path('frontend/erro404',views.erro404),
+    path('frontend/erro500', views.erro500),
+    path('frontend/login.html', views.login),
 ]
-
--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def erro404(request):
+    return render(request, 'erro404.html')
+def erro500(request):
+    return render(request, 'erro500.html')
+def login(request):
+    return render(request, 'frontend/login.html')
+def registrar(request):
+    return render(request, 'registrar.html')
