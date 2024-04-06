cf56a6756def3b2a15eb1175c7b7d517105e9d20
Francisco Quiñonez <Mr.qnns008@gmail.com>
2024-04-06 12:50:01 -0500

    templates80

--- gtb/urls.py
+++ gtb/urls.py
@@ -1,21 +1,94 @@
 from django.contrib import admin
-from django.urls import path,  include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),  
+    path('', views.home),
     path('home.html', views.home),
-
-
     path('frontend/escritorio', views.escritorio, name='escritorio'),
     path('frontend/erro404', views.erro404),
     path('frontend/erro500', views.erro500),
     path('frontend/login_view.html', views.login),
     path('frontend/registrar.html', views.registrar),
-
+    path('frontend/administrador/escritorio.html', views.admin_escritorio),
+    # Carpeta Cliente
+    path('frontend/cliente/escritorio', views.cliente_escrtorio),
+    path('frontend/cliente/plan', views.cliente_plan),
+    path('frontend/cliente/servicio', views.cliente_servicio),
+    # Carpeta Categoria
+    path('frontend/categoria/actualizar', views.categoria_actualizar),
+    path('frontend/categoria/eliminar', views.categoria_eliminar),
+    path('frontend/categoria/mostrar', views.categoria_mostrar),
+    path('frontend/categoria/nuevo', views.categoria_nuevo),
+    # Carpeta Clientes
+    path('frontend/clientes/actualizar', views.clientes_actualizar),
+    path('frontend/clientes/eliminar', views.clientes_eliminar),
+    path('frontend/clientes/importar', views.clientes_importar),
+    path('frontend/clientes/informacion', views.clientes_informacion),
+    path('frontend/clientes/mostrar', views.clientes_mostrar),
+    path('frontend/clientes/nuevo', views.clientes_nuevo),
+    # Carpeta Compra
+    path('frontend/compra/cancelar', views.compra_cancelar),
+    path('frontend/compra/informacion', views.comprar_informacion),
+    path('frontend/compra/mostrar', views.compra_mostrar),
+    path('frontend/compra/nuevo', views.compra_nuevo),
+    path('frontend/compra/ticket', views.compra_ticket),
+    # Carpeta Cuenta
+    path('frontend/cuenta/configuracion', views.cuenta_configuracion),
+    path('frontend/cuenta/perfil', views.cuenta_perfil),
+    path('frontend/cuenta/perfil2', views.cuenta_perfil2),
+    path('frontend/cuenta/salir', views.cuenta_salir),
+    # Carpeta Gastos
+    path('frontend/gastos/mostrar', views.gastos_mostrar),
+    path('frontend/gastos/nuevo', views.gastos_nuevo),
+    # Carpeta Graficos
+    path('frontend/graficos/mostrar', views.graficos_mostrar),
+    # Carpeta Plan
+    path('frontend/plan/actualizar', views.plan_actualizar),
+    path('frontend/plan/eliminar', views.plan_eliminar),
+    path('frontend/plan/foto', views.plan_foto),
+    path('frontend/plan/informacion', views.plan_informacion),
+    path('frontend/plan/mostrar', views.plan_mostrar),
+    path('frontend/plan/nuevo', views.plan_nuevo),
+    # Carpeta Producto
+    path('frontend/producto/actualizar', views.producto_actualizar),
+    path('frontend/producto/eliminar', views.producto_eliminar),
+    path('frontend/producto/foto', views.producto_foto),
+    path('frontend/producto/informacion', views.producto_informacion),
+    path('frontend/producto/mostrar', views.producto_mostrar),
+    path('frontend/producto/nuevo', views.producto_nuevo),
+    # Carpeta Reporte
+    path('frontend/reporte/clientes', views.reporte_clientes),
+    path('frontend/reporte/filtro-producto', views.reporte_filtro_producto),
+    path('frontend/reporte/filtro-servicio', views.reporte_filtro_servicio),
+    path('frontend/reporte/productos', views.reporte_productos),
+    path('frontend/ventas', views.reporte_ventas),
+    # Carpeta Servicio
+    path('frontend/servicio/actualizar', views.servicio_actualizar),
+    path('frontend/servicio/correo', views.servicio_correo),
+    path('frontend/servicio/eliminar', views.servicio_eliminar),
+    path('frontend/servicio/mostrar', views.servicio_mostrar),
+    path('frontend/servicio/nuevo', views.servicio_nuevo),
+    path('frontend/servicio/ticket', views.servicio_ticket),
+    path('frontend/servicio/ver', views.servicio_ver),
+    # Carpeta Usuario
+    path('frontend/usuario/actualizar', views.usuario_actualizar),
+    path('frontend/usuario/eliminar', views.usuario_eliminar),
+    path('frontend/usuario/mostrar', views.usuario_mostrar),
+    # Carpeta Venta
+    path('frontend/venta/cancelar', views.venta_cancelar),
+    path('frontend/venta/eliminar', views.venta_eliminar),
+    path('frontend/venta/informacion', views.venta_informacion),
+    path('frontend/venta/mostrar', views.venta_mostrar),
+    path('frontend/venta/nuevo', views.venta_nuevo),
+    path('frontend/venta/ticket', views.venta_ticket),
+    # Agrega aquí el resto de las URLs que faltan...
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tasks/views.py
+++ tasks/views.py
@@ -19,5 +19,199 @@ def escritorio(request):
 
 
 def login(request):
-    # Tu lógica aquí
     return render(request, 'frontend/login_view.html')
+#Carpeta Admin
+def admin_escritorio(request):
+    return render(request, 'frontend/administrador/escritorio.html')
+
+
+
+#Carpeta Categoria
+def categoria_actualizar(request):
+    return render(request, 'frontend/categoria/actualizar.html')
+
+def categoria_eliminar(request):
+    return render(request, 'frontend/categoria/eliminar.html')
+
+def categoria_mostrar(request):
+    return render(request, 'frontend/categoria/mostrar.html')
+
+def categoria_nuevo(request):
+    return render(request, 'frontend/categoria/nuevo.html')
+
+#Carpeta Cliente
+def cliente_escrtorio(request):
+    return render(request, 'frontend/cliente/escritorio.html')
+#plan
+#servicio
+
+def cliente_plan(request):
+    # Tu lógica aquí
+    return render(request, 'frontend/cliente/plan.html')
+
+def cliente_servicio(request):
+    return render(request, 'frontend/cliente/servicio.html')
+
+#Carpeta Clientes 
+def clientes_actualizar(request):
+    return render(request, 'frontend/clientes/actualizar.html')
+
+def clientes_eliminar(request):
+    return render(request, 'frontend/clientes/eliminar.html')
+
+def clientes_importar(request):
+    return render(request, 'frontend/clientes/importar.html')
+
+def clientes_informacion(request):
+    return render(request, 'frontend/clientes/informacion.html')
+
+def clientes_mostrar(request):
+    return render(request, 'frontend/clientes/mostrar.html')
+
+def clientes_nuevo(request):
+    return render(request, 'frontend/clientes/nuevo.html')
+
+#Carpeta Compra
+def compra_cancelar(request):
+    return render(request, 'frontend/compra/cancelar.html')
+
+def comprar_informacion(request):
+    return render(request, 'frontend/compra/informacion.html')
+
+def compra_mostrar(request):
+    return render(request, 'frontend/compra/mostrar.html')
+
+def compra_nuevo(request):
+    return render(request, 'frontend/compra/nuevo.html')
+
+def compra_ticket(request):
+    return render(request, 'frontend/compra/ticket.html')
+
+#Carpeta cuenta
+def cuenta_configuracion(request):
+    return render(request, 'frontend/cuenta/configuracion.html')
+
+def cuenta_perfil(request):
+    return render(request, 'frontend/cuenta/perfil.html')
+
+def cuenta_perfil2(request):
+    return render(request, 'frontend/cuenta/perfil2.html')
+
+def cuenta_salir(request):
+    return render(request, 'frontend/cuenta/salir.html')
+
+#Carpeta Gastos
+def gastos_mostrar(request):
+    return render(request, 'frontend/gastos/mostrar.html')
+
+def gastos_nuevo(request):
+    return render(request, 'frontend/gastos/nuevo.html')
+
+#Carpeta Graficos
+def graficos_mostrar(request):
+    return render(request, 'frontend/graficos/mostrar.html')
+
+#Carpeta Plan
+def plan_actualizar(request):
+    return render(request, 'frontend/plan/actualizar.html')
+
+def plan_eliminar(request):
+    return render(request, 'frontend/plan/eliminar.html')
+
+def plan_foto(request):
+    return render(request, 'frontend/plan/foto.html')
+
+def plan_informacion(request):
+    return render(request, 'frontend/plan/informacion.html')
+
+def plan_mostrar(request):
+    return render(request, 'frontend/plan/mostrar.html')
+
+def plan_nuevo(request):
+    return render(request, 'frontend/plan/nuevo.html')
+
+#Carpeta Producto
+def producto_actualizar(request):
+    return render(request, 'frontend/producto/actualizar.html')
+
+def producto_eliminar(request):
+    return render(request, 'frontend/producto/eliminar.html')
+
+def producto_foto(request):
+    return render(request, 'frontend/producto/foto.html')
+
+def producto_informacion(request):
+    return render(request, 'frontend/producto/informacion.html')
+
+def producto_mostrar(request):
+    return render(request, 'frontend/producto/mostrar.html')
+
+def producto_nuevo(request):
+    return render(request, 'frontend/producto/nuevo.html')
+
+#Carpeta Reporte
+def reporte_clientes(request):
+    return render(request, 'frontend/reporte/clientes.html')
+
+def reporte_filtro_producto(request):
+    return render(request, 'frontend/reporte/filtro-producto.html')
+
+def reporte_filtro_servicio(request):
+    return render(request, 'frontend/reporte/filtro-servicio.html')
+
+def reporte_productos(request):
+    return render(request, 'frontend/reporte/productos.html')
+
+def reporte_ventas(request):
+    return render(request, 'frontend/ventas.html')
+
+#Carpeta Servicio
+def servicio_actualizar(request):
+    return render(request, 'frontend/servicio/actualizar.html')
+
+def servicio_correo(request):
+    return render(request, 'frontend/servicio/correo.html')
+
+def servicio_eliminar(request):
+    return render(request, 'frontend/servicio/eliminar.html')
+
+def servicio_mostrar(request):
+    return render(request, 'frontend/servicio/mostrar.html')
+
+def servicio_nuevo(request):
+    return render(request, 'frontend/servicio/nuevo.html')
+
+def servicio_ticket(request):
+    return render(request, 'frontend/servicio/ticket.html')
+
+def servicio_ver(request):
+    return render(request, 'frontend/servicio/ver.html')
+
+#Carpeta Usuario
+def usuario_actualizar(request):
+    return render(request, 'frontend/actualizar.html')
+
+def usuario_eliminar(request):
+    return render(request, 'frontend/usuario/eliminar.html')
+
+def usuario_mostrar(request):
+    return render(request, 'frontend/usuario/mostrar.html')
+
+#Carpeta Venta
+def venta_cancelar(request):
+    return render(request, 'frontend/venta/cancelar.html')
+
+def venta_eliminar(request):
+    return render(request, 'frontend/venta/eliminar.html')
+
+def venta_informacion(request):
+    return render(request, 'frontend/venta/informacion.html')
+
+def venta_mostrar(request):
+    return render(request, 'frontend/venta/mostrar.html')
+
+def venta_nuevo(request):
+    return render(request, 'frontend/venta/nuevo.html')
+
+def venta_ticket(request):
+    return render(request, 'frontend/venta/ticket.html')
